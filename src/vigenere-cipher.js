8b4a67fa45413f08710fa0b157e6bede237e6df3
VeraEnders <veraabramo@gmail.com>
2022-10-13 03:04:27 +0200

    feat: add solution of vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,98 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ function prepareKeyword(str, key) {
+  let alphabetLatin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let newKey = '';
+  let spaces = 0;
+  while (key.length < str.length) {
+    key += key;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  str = str.toUpperCase();
+  for (let i = 0; i < str.length; i++) {
+      if (alphabetLatin.indexOf(str[i]) === -1) {
+          newKey += str[i];
+          spaces++;
+      } else {
+          newKey += key[i - spaces];
+      }
+  }
+  return newKey.toUpperCase();
+}
+
+function reverseStr(str) {
+  let newStr = ''; 
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+}
+
+class VigenereCipheringMachine {
+  constructor(condition) {
+    if (condition == false) {
+      this.condition = false;
+    } else {
+      this.condition = true;
+    }
+  }
+
+  encrypt(str, key) {
+    if (!str || !key) {
+      throw new Error ('Incorrect arguments!');
+    }
+    let alphabetLatin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let stringBeforeCrypt = str.toUpperCase();
+    let keyword = prepareKeyword(str, key);
+    let stringAfterCrypt = '';
+
+    for (let i = 0; i < stringBeforeCrypt.length; i++) {
+      let indexStr = alphabetLatin.indexOf(stringBeforeCrypt[i]);
+      let indexKey = alphabetLatin.indexOf(keyword[i]);
+      let indexInCryptStr = indexStr + indexKey;
+      if (indexStr === -1) {
+        stringAfterCrypt += stringBeforeCrypt[i];
+        continue;
+      } else if (indexInCryptStr > 25) {
+        indexInCryptStr = indexInCryptStr - 26;
+      } 
+      stringAfterCrypt += alphabetLatin[indexInCryptStr];
+    }
+
+    if (this.condition == false) {
+      return reverseStr(stringAfterCrypt);
+    } else {
+      return stringAfterCrypt;
+    } 
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) {
+      throw new Error ('Incorrect arguments!');
+    }
+    let alphabetLatin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let stringBeforeDecrypt = str.toUpperCase();
+    let keyword = prepareKeyword(str, key);
+    let stringAfterDecrypt = '';
+
+    for (let i = 0; i < stringBeforeDecrypt.length; i++) {
+      let indexInCryptStr = alphabetLatin.indexOf(stringBeforeDecrypt[i]);
+      let indexKey = alphabetLatin.indexOf(keyword[i]);
+      let indexInStr = indexInCryptStr - indexKey;
+      if (indexInCryptStr === -1) {
+        stringAfterDecrypt += stringBeforeDecrypt[i];
+        continue;
+      } else if (indexInStr < 0) {
+        indexInStr = indexInStr + 26;
+      } 
+      stringAfterDecrypt += alphabetLatin[indexInStr];
+    }
+
+    if (this.condition == false) {
+      return reverseStr(stringAfterDecrypt);
+    } else {
+      return stringAfterDecrypt;
+    }
   }
 }
 
